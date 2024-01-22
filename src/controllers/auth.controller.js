6bbd075f4d8f2d12143a651b365bd74d7217ff9f
clsgon <hercarlos@hotmail.es>
2024-01-22 19:36:00 +0100

    continue

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,24 @@
 const User = require("../models/User")
 
+export async function getUserByEmail(email){
+    try{
+        return User.find({"email":email})
+    }catch(error){
+        console.log(error)
+    }
+    return null
+}
+
 export async function login(req, res){
-    const {user, password} = req.body
-    if (user === 'adrian' && password === '12345678') {
-        return res.json({success: 'El es adri'})
+    const {email, password} = req.body
+    const user = await getUserByEmail(email)
+    if (user) {
+        return res.json(user)
+            //return res.json({success: "Us"})
     }
     return res.status(500).json({error: "Usuario y contraseña no coinciden"})
 }   
+
+export async function signup(req, res){
+
+}
--- src/requests/auth.request.js
+++ src/requests/auth.request.js
@@ -1,6 +1,6 @@
 export function login(req, res, next){
     const { body } = req
-    const requiredFields = ['user', 'password']
+    const requiredFields = ['email', 'password']
     const emptyFields = requiredFields.filter((field) => !body[field])
     if (emptyFields.length > 0)
         return res.status(400).json({error: "Faltan campos obligatorios", emptyFields})
