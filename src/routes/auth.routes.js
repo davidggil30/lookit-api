b48a41340a470b862730af15e39b9149b5e3eaa4
clsgon <hercarlos@hotmail.es>
2024-04-10 11:14:54 +0200

    changes

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const authController = require("../controllers/auth.controller")
-const authRequest = require("../requests/auth.request")
-
-router.post("/signup", authRequest.signup, authController.signup)
-router.post("/signin", authRequest.signin, authController.signin)
-
-export default router
--- src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const usersController = require("../controllers/users.controller")
-
-
-router.post("/getUserByEmail", usersController.getUserByEmail)
-
-export default router
