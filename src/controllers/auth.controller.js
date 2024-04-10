2a6a15d34e13da61053bc71007a3f505a02d0645
clsgon <hercarlos@hotmail.es>
2024-04-10 10:49:49 +0200

    changes

--- src/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-const Film = require('../models/Film')
-
-export async function login(req, res){
-    const {user, password} = req.body
-    if (user === 'adrian' && password === '12345678') {
-        return res.json({success: 'El es adri'})
-    }
-    return res.status(500).json({error: "Usuario y contraseña no coinciden"})
-}
