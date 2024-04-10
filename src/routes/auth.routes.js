eb7a1f393824df5ffa3dae1c1ab1333a0fbcff53
clsgon <hercarlos@hotmail.es>
2024-04-10 10:54:30 +0200

    pr

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const authController = require("../controllers/auth.controller")
-const authRequest = require("../requests/auth.request")
-
-router.post("/login", authRequest.login, authController.login)
-
-export default router
