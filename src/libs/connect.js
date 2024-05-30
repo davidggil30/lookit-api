266b4b500b6965b4845eb56b46c9a8e42a328bbd
clsgon <hercarlos@hotmail.es>
2024-05-30 18:29:38 +0200

    connect atlas

--- src/libs/connect.js
+++ src/libs/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const dbname = "lookit"
-const uri = "mongodb://localhost:27017"
+const uri = "mongodb+srv://clgon:clgon@clustercarlos.wrsofji.mongodb.net/?retryWrites=true&w=majority&appName=ClusterCarlos"
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Conectado a mongodb"))
